cb55570f20b94756a2a8eeb95cdbcbd24e58c3f5
yalab <rudeboyjet@gmail.com>
2022-09-30 02:25:08 +0900

    Implement the one that changes messages at the touch of a button.

--- app/src/main/java/org/yalab/milk/ui/home/HomeFragment.kt
+++ app/src/main/java/org/yalab/milk/ui/home/HomeFragment.kt
@@ -2,9 +2,11 @@ package org.yalab.milk.ui.home
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.core.view.MotionEventCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import org.yalab.milk.databinding.FragmentHomeBinding
@@ -27,7 +29,7 @@ class HomeFragment : Fragment() {
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
-
+        initLightButtion()
         val textView: TextView = binding.textHome
         homeViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
@@ -38,5 +40,24 @@ class HomeFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    fun initLightButtion() {
+        binding.LightButton.setOnTouchListener { v, event ->
+            val action: Int = MotionEventCompat.getActionMasked(event)
+            when (action) {
+                MotionEvent.ACTION_DOWN -> {
+                    binding.textHome.text = "ACTION_DOWN"
+                    true
+                }
+                MotionEvent.ACTION_UP -> {
+                    binding.textHome.text = "ACTION_UP"
+                    true
+                }
+                else -> {
+                    false
+                }
+            }
+        }
     }
 }
