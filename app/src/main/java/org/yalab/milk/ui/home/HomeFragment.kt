adf4f54daf9546629ed5e19d9ab5e18902ce5cbc
yalab <rudeboyjet@gmail.com>
2022-10-04 01:48:31 +0900

    Camera flash can now be turned on when permissions are manually granted.

--- app/src/main/java/org/yalab/milk/ui/home/HomeFragment.kt
+++ app/src/main/java/org/yalab/milk/ui/home/HomeFragment.kt
@@ -2,11 +2,17 @@ package org.yalab.milk.ui.home
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.camera.core.CameraControl
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.content.ContextCompat
 import androidx.core.view.MotionEventCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
@@ -14,12 +20,16 @@ import org.yalab.milk.R
 import org.yalab.milk.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
+    companion object {
+        private const val TAG = "Milk"
+    }
 
     private var _binding: FragmentHomeBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+    lateinit var cameraControl: CameraControl
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -39,6 +49,11 @@ class HomeFragment : Fragment() {
         return root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        startCamera()
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
@@ -50,11 +65,13 @@ class HomeFragment : Fragment() {
             val action: Int = event.action
             when (action) {
                 MotionEvent.ACTION_DOWN -> {
+                    cameraControl.enableTorch(true)
                     binding.textHome.text = getString(R.string.light_button_down)
                     v.performClick()
                     true
                 }
                 MotionEvent.ACTION_UP -> {
+                    cameraControl.enableTorch(false)
                     binding.textHome.text = getString(R.string.light_button_up)
                     true
                 }
@@ -63,5 +80,26 @@ class HomeFragment : Fragment() {
                 }
             }
         }
+    }
+
+    private fun startCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(requireContext())
+        cameraProviderFuture.addListener({
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+            val preview = Preview.Builder()
+                .build()
+                .also {
+                    it.setSurfaceProvider(binding.viewFinder.surfaceProvider)
+                }
+            try {
+                cameraProvider.unbindAll()
+                val camera = cameraProvider.bindToLifecycle(
+                    this, CameraSelector.DEFAULT_BACK_CAMERA, preview
+                )
+                cameraControl = camera.cameraControl
+            } catch (exc: Exception) {
+                Log.e(TAG, "Use case binding failed", exc)
+            }
+        }, ContextCompat.getMainExecutor(requireContext()))
     }
 }
