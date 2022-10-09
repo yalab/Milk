55b54feb2fdbc9b9d56b6c9a87b6572e8d38ebba
yalab <rudeboyjet@gmail.com>
2022-10-09 14:58:37 +0900

    create data/Light.kt

---
+++ app/src/main/java/org/yalab/milk/data/Light.kt
@@ -0,0 +1,4 @@
+package org.yalab.milk.data
+
+class Light {
+}
---
+++ app/src/test/java/org/yalab/milk/data/LightTest.kt
@@ -0,0 +1,12 @@
+package org.yalab.milk.data
+
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Test
+
+class LightTest{
+    @Test
+    fun on() {
+        assertEquals(1, 1)
+    }
+}
