0d0f2c5e10dffd0037fa905e047a005f99948cf4
yalab <rudeboyjet@gmail.com>
2023-01-04 16:37:20 +0900

    launchFragmentInContainer can now be used.

--- app/src/androidTest/java/org/yalab/milk/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/org/yalab/milk/ExampleInstrumentedTest.kt
@@ -1,5 +1,6 @@
 package org.yalab.milk
 
+import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
 
@@ -7,6 +8,7 @@ import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.yalab.milk.ui.home.HomeFragment
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -18,7 +20,9 @@ class ExampleInstrumentedTest {
     @Test
     fun useAppContext() {
         // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("org.yalab.milk", appContext.packageName)
+//        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        val scenario = launchFragmentInContainer<HomeFragment>()
+        assertEquals(true, true)
+//        assertEquals("org.yalab.milk", appContext.packageName)
     }
 }
--- app/src/androidTest/java/org/yalab/milk/domain/LightTest.kt
+++ app/src/androidTest/java/org/yalab/milk/domain/LightTest.kt
@@ -1,22 +1,5 @@
-package org.yalab.milk.data
+package org.yalab.milk.domain
 
-import androidx.lifecycle.LifecycleOwner
 import org.junit.Assert.*
-import org.junit.Before
-import org.junit.Test
 
-class LightTest{
-//    lateinit var light:Light
-
-    @Before
-    @Throws(Exception::class)
-    fun setUp() {
-//        light = Light()
-    }
-
-    @Test
-    fun on() {
-//        assertEquals(true, light.on())
-        assertEquals(true, true)
-    }
-}
+class LightTest
